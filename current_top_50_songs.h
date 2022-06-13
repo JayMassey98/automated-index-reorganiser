5d69f8171915db10480a7f6f6cbcccf47df4f29c
Jay Massey <jaymassey98@gmail.com>
2022-06-13 12:17:03 +0100

    Added file generation to update an index with parsed HTML data.

---
+++ current_top_50_songs.h
@@ -0,0 +1,14 @@
+// ---------------------------------------------------------------------------
+// This project is a self-taught exercise. For more examples of various coding
+// projects, please see my GitHub portfolio at https://github.com/JayMassey98.
+// ---------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------
+// PURPOSE: Declares an array to store a user's current top 50 Spotify songs.
+// --------------------------------------------------------------------------
+
+#pragma once
+
+#define STRING_LENGTH_MAX 1000
+
+extern const char current_top_50_songs[50][STRING_LENGTH_MAX + 1];
